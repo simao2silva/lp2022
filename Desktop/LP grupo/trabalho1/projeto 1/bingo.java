0b5575f6e69453783b0d49ffa8752344680bd7ee
simao2silva <simao.silva@ua.pt>
2021-03-23 18:52:22 +0000

    loto_inicio

--- Desktop/LP grupo/trabalho1/projeto 1/bingo.java
+++ Desktop/LP grupo/trabalho1/projeto 1/bingo.java
@@ -1,3 +1,27 @@
-public class App {
+import java.util.Locale;
+import java.util.Scanner;
 
+public class bingo {
+    int  folheto[][] = new int [3][9];
+    int numero;
+    Scanner scan = new Scanner(System.in);
+    int numFolheto;
+ 
+    // esta parte serve para introduzir o numero que foi gerado
+    System.out.println("Digite aqui o número que foi sorteado");
+    numero = scan.nextInt();
+    if(numero<1 && numero>89){
+        System.out.println("Número sorteado invalido por favor tente de novo!");
+
+    }else if (numero = numFolheto){
+        // aqui falta o marcador para se colocar no folheto
+    }
+    
+    
+   
+// falta fazer um mecanismo que gere 15 numeros
+// tem que gerar numero de 1 a 89, 5vezes por linha e so pode ter as dezenas repetidas no max 3 vezes
+
+
+    
 }
