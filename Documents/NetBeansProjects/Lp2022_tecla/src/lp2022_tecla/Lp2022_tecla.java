20c397577b8d20711a00bd3932eecd7b5223c58b
simao2silva <simao.silva@ua.pt>
2022-03-15 14:58:52 +0000

    novo commit

---
+++ Documents/NetBeansProjects/Lp2022_tecla/src/lp2022_tecla/Lp2022_tecla.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lp2022_tecla;
+
+/**
+ *
+ * @author simao
+ */
+public class Lp2022_tecla {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
